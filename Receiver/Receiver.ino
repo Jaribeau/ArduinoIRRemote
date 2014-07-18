ffc4fd1c7668f5d45631b331a8dc027903d89e48
Jaribeau <jared.baribeau@gmail.com>
2014-07-18 15:16:14 -0400

    "Set up serial and IO pins"

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,31 +1,49 @@
+
 /******************************************/
 /********* SYDE 192 - Team Blonde *********/
 /********** ARDUINO 2 - RECEIVER **********/
 /******************************************/
+#define ON 1
+#define OFF 0
 
+boolean ledMatrixStates[];
+String serialIn;
+
+  
 /* Steup Function ****************************/
 void setup() {
   
   //Init IR Serial and RX pin
+  Serial.begin(2400);  //Start serial with baud rate of 2400
   
   //Init LED matrix and pins
+  pinMode(4, INPUT);
+  pinMode(4, INPUT);
+  pinMode(4, INPUT);
+  pinMode(4, INPUT);
+  pinMode(4, INPUT);
+  pinMode(4, INPUT);
+  ledMatrixStates[] = {OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF};
   
   //Init interrupt
+  //attachInterrupt(0, interruptHandle, HIGH);
+  
   
 }//End of setup()
 
 
 /* Main Loop ********************************/
 void loop() {
-  //Rotate through which pin is enabled each iteration
+  //Read IR serial input and make appropriate volume, or mute adjustments
+  serialIn = Serial.read();
+  
+  //Rotate through which pin is turned on
     
 }//End of loop()
 
 
-/* Interrupt Handle for wake/sleep ****************************8/
+/* Interrupt Handle *****************************/
 void interruptHandle() {
-  //Check for which button pressed
-  
-  //Send corresponding message to IR blaster
+  //Wake up or sleep arduino on interrupt
 
 }//End of interruptHandle()
--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -1,3 +1,4 @@
+
 /******************************************/
 /********* SYDE 192 - Team Blonde *********/
 /********** ARDUINO 1 - REMOTE ************/
@@ -6,12 +7,17 @@
 /* Steup Function ********************/
 void setup() {
   
-  //Init IR Serial and TX pin
+  //Init IR/Serial TX pin
+  Serial.begin(2400);
   
   //Init button pins
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
   
   //Init interrupt
-  
+  attachInterrupt(0, interruptHandle, HIGH);
   
 }//End of setup()
 
@@ -25,8 +31,14 @@ void loop() {
 
 /* Interrupt Handle for button presses ****************************/
 void interruptHandle() {
-  //Check for which button pressed
-  
-  //Send corresponding message to IR blaster
+  //Check for which button pressed and send corresponding IR code
+  if(digitalRead(4))
+     Serial.send("<Insert hex code here>");    //send power IR code
+  else if(digitalRead(5))
+     Serial.send("<Insert hex code here>");        //send volume up IR code
+  else if(digitalRead(6))
+     Serial.send("<Insert hex code here>");        //send volume down IR code
+  else if(digitalRead(7))
+     Serial.send("<Insert hex code here>");        //send mute IR code
 
 }//End of interruptHandle()
