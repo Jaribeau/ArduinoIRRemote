3d40d84b30faa10935117f1944390e36626a432f
Jaribeau <jared.baribeau@gmail.com>
2014-07-17 21:56:03 -0400

    Initial Commit, Receiver and Remote Sketches added

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,31 @@
+/******************************************/
+/********* SYDE 192 - Team Blonde *********/
+/********** ARDUINO 2 - RECEIVER **********/
+/******************************************/
+
+/* Steup Function ****************************/
+void setup() {
+  
+  //Init IR Serial and RX pin
+  
+  //Init LED matrix and pins
+  
+  //Init interrupt
+  
+}//End of setup()
+
+
+/* Main Loop ********************************/
+void loop() {
+  //Rotate through which pin is enabled each iteration
+    
+}//End of loop()
+
+
+/* Interrupt Handle for wake/sleep ****************************8/
+void interruptHandle() {
+  //Check for which button pressed
+  
+  //Send corresponding message to IR blaster
+
+}//End of interruptHandle()
---
+++ Remote/Remote.ino
@@ -0,0 +1,32 @@
+/******************************************/
+/********* SYDE 192 - Team Blonde *********/
+/********** ARDUINO 1 - REMOTE ************/
+/******************************************/
+
+/* Steup Function ********************/
+void setup() {
+  
+  //Init IR Serial and TX pin
+  
+  //Init button pins
+  
+  //Init interrupt
+  
+  
+}//End of setup()
+
+
+/* Main Loop *********************/
+void loop() {
+  //Not actually sure if there's anything for us to do in here... oh well.
+    
+}//End of loop()
+
+
+/* Interrupt Handle for button presses ****************************/
+void interruptHandle() {
+  //Check for which button pressed
+  
+  //Send corresponding message to IR blaster
+
+}//End of interruptHandle()
