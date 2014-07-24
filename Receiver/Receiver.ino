a9cfa2ef21cd167ab122b944425eb9ec124a5154
Andrew <andrew.smiles@outlook.com>
2014-07-23 23:06:05 -0400

    Sleep Implemented

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -71,29 +71,7 @@ void setup() {
   pinMode(col1, OUTPUT);
   pinMode(col2, OUTPUT);
   pinMode(col3, OUTPUT);    
-  
-  //Init interrupt
-  //attachInterrupt(0, interruptHandle, HIGH);
 
-  /* Arduino Sleep Setup */
-  pinMode(interruptPin, OUTPUT);
-  pinMode(wakePin, INPUT);
-
-  /* Enable an interrupt. In the function call 
-  attachInterrupt(A, B, C)
-  A   can be either 0 or 1 for interrupts on pin 2 or 3.   
-  B   Name of a function you want to execute while in interrupt A.
-  C   Trigger mode of the interrupt pin. Can be:
-  LOW - a low level trigger
-  CHANGE - a change in level trigger
-  RISING - a rising edge of a level trigger
-  FALLING - a falling edge of a level trigger
-
-  All sleep modes use LOW except for the IDLE sleep mode.
-  */
-
-  // use pin 2, interrupt 0, to run function wakeUpNow when pin 2 gets LOW. 
-  attachInterrupt(0, wakeUpNow, LOW);
 }//End of setup()
 
 
@@ -109,12 +87,7 @@ void loop() {
   if(ledScrollState > 9)
     ledScrollState = 1;
 
-  /* Arduino sleep code */
-  // read sleep pin here. only active                          
-  sleepStatus = digitalRead(sleepPin);
-  //when blink led is off, start to put the device in sleep, call sleep function                                       
-  if (sleepStatus == LOW) 
-    sleepNow();
+  
     
 }//End of loop()
 
@@ -146,8 +119,10 @@ void refreshState()
         
         //Update variable corresponding to command received
         if(serialIn.equals("Yellow>"))
-          power = !power;
-        else if(serialIn == "Red>" && volume < 8)
+        {
+          sleepNow();
+        }
+        else if(serialIn.equals("Red>") && volume < 8)
         {
             volume++;
             mute = false;
@@ -272,14 +247,11 @@ void setAllLEDsLow(){
 void sleepNow()         
 {
   /* 
-  Set sleep mode. In the Atmega8 datasheet
-  http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf pg35
-  there is a list of sleep modes that explains which clocks and 
-  wake up sources are available in each sleep modus.
+
 
   In the avr/sleep.h file, the call names of each sleep modus are found:
   The 5 different modes are:
-    SLEEP_MODE_IDLE         -the least power savings 
+  **SLEEP_MODE_IDLE**       -the least power savings 
     SLEEP_MODE_ADC
     SLEEP_MODE_PWR_SAVE
     SLEEP_MODE_STANDBY
@@ -287,33 +259,22 @@ void sleepNow()
   Want as much power savings as possible, so use SLEEP_MODE_PWR_DOWN
   */
 
+  setAllLEDsLow();
+  Serial.println("Going to sleeeeeeep!!");
+  
   // sleep mode is set here
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  set_sleep_mode(SLEEP_MODE_IDLE);
 
   // enables the sleep bit in the mcucr register so sleep is possible. a safety pin.
-  sleep_enable();              
-  /* Enable an interrupt. address case where interrupt is pushed accidentally. ensure that accidentally pushed the interrupt button doesn't interrupt our running program. To run interrupt code other than the sleep function, place it in the setup(). 
-  In the function call attachInterrupt(A, B, C)
-  A   can be either 0 or 1 for interrupts on pin 2 or 3.   
-  B   Name of a function you want to execute at interrupt for A.
-  C   Trigger mode of the interrupt pin. can be:
-    LOW        a low level triggers
-    CHANGE     a change in level triggers
-    RISING     a rising edge of a level triggers
-    FALLING    a falling edge of a level triggers
-
-   All sleep modes use LOW except for the IDLE sleep mode.
-   */
-   // use interrupt 0 (pin 2) and run function wakeUpNow when pin 2 gets LOW 
-  attachInterrupt(0,wakeUpNow, LOW); 
-  // put arduino to sleep
+  sleep_enable();
+  delay(1000);
+  PRR = PRR | 0b00100000;  //Disables Timer/Counter0 to prevent waking up
   sleep_mode();                
   // first thing arduino does after waking from sleep: disable sleep
   sleep_disable();             
-  // disable interrupt 0 on pin 2 so the wakeUpNow code will not be executed during normal run time
-  detachInterrupt(0);          
-  // wait 2 secs to allow people to notice the interrupt. light the LED to show that the interrupt is handled.
-  delay(1000);                 
+  PRR = PRR & 0b00000000;  //Re-enables Timer/Counter0 to resume functionality
+         
+    delay(1000);                 
   // turn off the interrupt LED
-  digitalWrite (interruptPin, LOW);
+
 }
